30d156e6ee285f08a6fa162555d67ef6e41b632b
Seatis <atikezer@gmail.com>
2017-10-06 11:36:25 +0200

    feat(basic-exam-candy_shop): candy_shop is done

--- candyshop/candy_shop.py
+++ candyshop/candy_shop.py
@@ -17,24 +17,87 @@
 # The CandyShop should be represented as string in this format:
 # "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
 
+class Candie(object):
+    
+    def __init__(self):
+        self.price = 10
+        self.sugar = 5
+        self.name = "candie"
+
+
+class Lollipop(object):
+    
+    def __init__(self):
+        self.price = 20
+        self.sugar = 10
+        self.name = "lollipop"
+
+
+class CandyShop(object):
+
+    candy_shop_storage = []
+    income = 0
+
+    def __init__(self, amount_of_sugar):
+        self.amount_of_sugar = amount_of_sugar
+
+    def create_sweets(self, sweet_type):
+        if sweet_type == "candy":
+            self.candy_shop_storage.append(Candie())
+            self.amount_of_sugar -= self.candy_shop_storage[-1].sugar
+        elif sweet_type == "lollipop":
+            self.candy_shop_storage.append(Lollipop())
+            self.amount_of_sugar -= self.candy_shop_storage[-1].sugar
+
+    def raise_prices(self, percentage):
+        for sweet in self.candy_shop_storage:
+            sweet.price = sweet.price + sweet.price * (percentage / 100)
+
+    def sweet_number_calc(self, sweet_type):
+        self.sweet_number = 0
+        for sweet in self.candy_shop_storage:
+            if sweet.name == sweet_type:
+                self.sweet_number += 1
+        return self.sweet_number
+
+    def sell(self, sweet_type, amount):
+        while amount > 0 and self.sweet_number_calc(sweet_type) != 0:
+            for i in range(len(self.candy_shop_storage)):
+                if self.candy_shop_storage[i].name == sweet_type:
+                    self.income += self.candy_shop_storage[i].price
+                    del self.candy_shop_storage[i]
+                    amount -= 1
+                    break
+
+    def buy_sugar(self, amount):
+        self.amount_of_sugar += amount
+        self.income -= ( amount / 1000 ) * 100
+
+
+    def __repr__(self):
+        return "Inventory: {} candies, {} lollipops, Income: {}, Sugar: {}".format(self.sweet_number_calc("candie"), self.sweet_number_calc("lollipop"), self.income, self.amount_of_sugar)
+        
+
+
+
 candy_shop = CandyShop(300)
 candy_shop.create_sweets("candy")
 candy_shop.create_sweets("candy")
+candy_shop.create_sweets("candy")
 candy_shop.create_sweets("lollipop")
 candy_shop.create_sweets("lollipop")
+candy_shop.create_sweets("candy")
 print(candy_shop)
-# Should print out:
-# Invetory: 2 candies, 2 lollipops, Income: 0, Sugar: 270gr
-candy_shop.sell("candy", 1)
-print(candy_shop
-# Should print out:
-# "Invetory: 1 candies, 2 lollipops, Income:20, Sugar: 285gr"
+
+candy_shop.sell("candie", 2)
+print(candy_shop)
+
+candy_shop.buy_sugar(30)
+print(candy_shop)
+
+candy_shop.sell("lollipop", 1)
+print(candy_shop)
+
 candy_shop.raise_prices(5)
 candy_shop.sell("lollipop", 1)
 print(candy_shop)
-# Should print out:
-# "Invetory: 1 candies, 1 lollipops, Income:35, Sugar: 285gr"
-candy_shop.buy_sugar(300)
-print(candy_shop)
-# Should print out:
-# "Invetory: 1 candies, 1 lollipops, Income:5, Sugar: 315gr"
