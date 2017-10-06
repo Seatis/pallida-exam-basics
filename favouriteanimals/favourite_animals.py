66b87bcb18f8e0eb54790209c793716cb3626398
Seatis <atikezer@gmail.com>
2017-10-06 10:00:32 +0200

    feat(basic-exam-favourite_animals): created command selector

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -7,3 +7,31 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+import sys
+
+def command_selector():
+    if len(sys.argv) == 1:
+        print("fav_animals [animal] [animal]")
+    else:
+        collect_unique_animals(sys.argv[1:])
+
+
+def collect_unique_animals(favourite_animal_list):
+    unique_animal_list = []
+    for favourite_animal in favourite_animal_list:
+        flag = True
+        for unique_animal in unique_animal_list:
+            if favourite_animal == unique_animal:
+                flag = False
+        if flag:
+            unique_animal_list.append(favourite_animal)
+    store_unique_animals(unique_animal_list)
+
+def store_unique_animals(unique_animals):
+    animals_text = ""
+    for animal in unique_animals:
+        animals_text += animal + "\n"
+    print(animals_text)
+
+command_selector()
+
